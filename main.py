052edb1e07efd558399be1e1d76bc32ef5f4b515
AlexGoodman <>
2021-09-04 08:57:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+course = 'Python for Begginers'
+print(type(course))
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('P'))
+print (course.replace('Begginers', 'Absolute Begginers'))
+print('Python' in course)
